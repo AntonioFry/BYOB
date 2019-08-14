bb0f155197635e7a6db167fd0fb2730ee466c660
Antonio Fry <antoniofry2000@gmail.com>
2019-08-14 08:52:17 -0600

    add PORT to app file

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
+const PORT = process.env.PORT || 3000
 const app = express();
 
 app.get('/', (request, response) => response.send('Hello World'));
 
-app.listen(3000, () => console.log('app is running on port 3000'));
+app.listen(PORT, () => console.log('app is running'));
