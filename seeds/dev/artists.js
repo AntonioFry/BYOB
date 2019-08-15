1cb2bfd00ffeaad5848780163801821a814fa586
Antonio Fry <antoniofry2000@gmail.com>
2019-08-14 20:24:59 -0600

    add seeding functionality

--- seeds/dev/artists.js
+++ seeds/dev/artists.js
@@ -1,14 +1,53 @@
+const albumsData = require('../../data/albumsData');
+const artistsData = require('../../data/artistsData');
 
-const createArtist = (knex, paper) => {
 
+const createArtist = (knex, artist) => {
+  return knex("artists").insert({
+    "artist_name": artist.artist_name,
+    "genre": artist.genre,
+    "member_count": artist.member_count,
+    "year_formed": artis.year_formed
+  }, "id")
+  .then(artistId => {
+    let albumPromises = [];
+
+    artist.albums.forEach(album => {
+      const foundAlbum = findAlbum(album);
+      albumPromises.push(
+        createAlbum(knex, {
+          ...foundAlbum,
+          artist_id: artistId[0] 
+        })
+      )
+    });
+
+    return Promise.all(albumPromises);
+  })
+}
+
+const findAlbum = (currentAlbum) => {
+  return albumsData.find(album => {
+    return album.album_name === currentAlbum;
+  })
 }
 
 const createAlbum = (knex, album) => {
-
+  return knex("albums").insert(album);
 }
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
   return knex('albums').del()
     .then(() => knex('artists').del())
+  .then(() => {
+    let artistPromises = [];
+
+    artistsData.forEach(artist => {
+      artistPromises.push(createArtist(knex, artist));
+    });
+
+    return Promise.all(artistPromises);
+  })
+  .catch(error => console.log(`Error seeding data: ${error}`));
 };
