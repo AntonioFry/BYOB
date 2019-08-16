a7ec0442c2f10b4ffcb58f10fb6d797244a8e0d9
Antonio Fry <antoniofry2000@gmail.com>
2019-08-16 09:39:19 -0600

    edit configs

--- app.js
+++ app.js
@@ -9,71 +9,74 @@ app.set('port', PORT)
 app.get('/', (request, response) => response.send('Hello World'));
 app.use(express.json());
 app.use(express.static('public'));
-app.listen(PORT, () => console.log('app is running'));
 
 // endpoints
-  // GET artists
-  app.get('/api/v1/artists', (request, response) => {
-    database('artists').select()
-      .then((artists) => {
-        response.status(200).json(artists)
-      })
-      .catch(error => {
-        response.status(500).json({ error })
-      })
+// GET artists
+app.get('/api/v1/artists', (request, response) => {
+  database('artists').select()
+  .then((artists) => {
+    response.status(200).json(artists)
   })
-
-  // GET artists by id
-  app.get('/api/v1/artists/:id', (request, response) => {
-    const { id } = request.params;
-    database('artists').select()
-      .then((artists) => {
-        const foundArtist = artists.find(artist => artist.id === parseInt(id));
-        if (foundArtist !== undefined) {
-          return response.status(200).json(foundArtist);
-        } else {
-          return response.sendStatus(404);
-        }
-      })
-      .catch((error) => {
-        response.status(500).json({ error: error.message });
-      })
+  .catch(error => {
+    response.status(500).json({ error })
   })
-  
-  // GET albums
-    app.get('/api/v1/albums', (request, response) => {
-      database('albums').select()
-        .then((albums) => {
-          response.status(200).json(albums)
-        })
-        .catch((error) => {
-          response.status(500).json({ error: error.message });
-        })
-    })
+})
 
-
-  // GET albums by id
-  app.get('/api/v1/albums/:id', (request, response) => {
-    const { id } = request.params;
-    database('albums').select()
-      .then((albums) => {
-        const foundAlbum = albums.find(album => album.id === parseInt(id));
-        if (foundAlbum !== undefined) {
-          return response.status(200).json(foundAlbum);
-        } else {
-          return response.sendStatus(404);
-        }
-      })
-      .catch((error) => {
-        response.status(500).json({ error: error.message });
-      })
+// GET artists by id
+app.get('/api/v1/artists/:id', (request, response) => {
+  const { id } = request.params;
+  database('artists').select()
+  .then((artists) => {
+    const foundArtist = artists.find(artist => artist.id === parseInt(id));
+    if (foundArtist !== undefined) {
+      return response.status(200).json(foundArtist);
+    } else {
+      return response.sendStatus(404);
+    }
   })
+  .catch((error) => {
+    response.status(500).json({ error: error.message });
+  })
+})
 
-  // POST artists
+// GET albums
+app.get('/api/v1/albums', (request, response) => {
+  database('albums').select()
+  .then((albums) => {
+    response.status(200).json(albums)
+  })
+  .catch((error) => {
+    response.status(500).json({ error: error.message });
+  })
+})
 
 
-  // POST albums
+// GET albums by id
+app.get('/api/v1/albums/:id', (request, response) => {
+  const { id } = request.params;
+  database('albums').select()
+  .then((albums) => {
+    const foundAlbum = albums.find(album => album.id === parseInt(id));
+    if (foundAlbum !== undefined) {
+      return response.status(200).json(foundAlbum);
+    } else {
+      return response.sendStatus(404);
+    }
+  })
+  .catch((error) => {
+    response.status(500).json({ error: error.message });
+  })
+})
+
+// POST artists
+app.post('api/v1/albums', (request, response) => {
+  const { artist_name, genre, member_count, year_formed } = 
+})
+
+// POST albums
 
 
-  // DELETE artist
+// DELETE artist
 
+
+app.listen(PORT, () => console.log('app is running'));
--- knexfile.js
+++ knexfile.js
@@ -12,6 +12,15 @@ module.exports = {
     seeds: {
       directory: './seeds/dev'
     }
+  },
+
+  production: {
+    client: 'pg',
+    useNullAsDefault: true,
+    connection: process.env.DATABASE_URL + `?ssl=true`,
+    migrations: {
+      directory: './migrations'
+    }
   }
 
 };
